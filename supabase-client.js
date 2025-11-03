771085be274c6f589b443db1140b4d10ee256ffa
Nenê Transportes <nenecinetransportes@gmail.com>
2025-11-03 17:03:32 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,10 +1,11 @@
 // --- supabase-client.js ---
+// Erro corrigido com +esm e suas chaves
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// 1. Cole a URL do seu projeto Supabase aqui
-const SUPABASE_URL = 'URL_DO_SEU_PROJETO_SUPABASE'; 
-// 2. Cole a Chave "anon public" do seu projeto aqui
-const SUPABASE_KEY = 'SUA_CHAVE_ANON_PUBLIC';
+// 1. Sua URL do Supabase
+const SUPABASE_URL = 'https://wsbdhavigcabcnqlrdnl.supabase.co'; 
+// 2. Sua Chave "anon public"
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzYmRoYXZpZ2NhYmNucWxyZG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxOTI0ODcsImV4cCI6MjA3Nzc2ODQ4N30.fXy3oFmv1Zv8CKBNUrAYrNfymv4zA09IcULVsXQ--i8';
 
 // 3. Exporta o cliente Supabase para ser usado em outros arquivos
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
